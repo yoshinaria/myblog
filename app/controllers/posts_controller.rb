f1ac7f39f424313480f871f71d233e04e021a29b
Akiko <yoshinariakiko@gmail.com>
2015-05-08 19:58:39 +0000

    Fixed link issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
-   def index
+  def index
     @posts = Post.all
   end
+
+  def show
+    @posts = Post.find(:id)
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def delete
+    
+  end
+
 end
